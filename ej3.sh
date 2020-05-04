c02d2908a51c9e73efe8c0af7c8f7829245d652d
alumno <alumno@pc-alumno.(none)>
2020-05-04 19:01:11 +0200

    Último ejercicio practicamente terminado

--- ej3.sh
+++ ej3.sh
@@ -1,5 +1,14 @@
+ficheros=0
+directorios=0
+
 read -p "Dime el directorio donde quieres buscar: " dir
 
+for i in `ls -l $dir | awk '{print $1}'`; do
+	if [ $i = "d*" ]; then
+		directorios=$(($directorios+1))
+	elif [ $i = "-*" ]; then
+		ficheros=$(($ficheros+1))
+	do
+done
 
-
-ls $dir
+echo "En este directorio hay un total de $ficheros ficheros y $directorios "
