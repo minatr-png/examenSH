69ed289bea16dfd5f75fde2280b0dc707b23e11a
alumno <alumno@pc-alumno.(none)>
2020-05-04 18:31:16 +0200

    Estoy haciendo el segundo ejercicio (se responde en florida), el primero ya está terminado

--- ej1.sh
+++ ej1.sh
@@ -1,15 +1,18 @@
 if [ $# -ne 1 ]; then
 	echo "Error. Necesitas pasar un parámetro"
+	exit
 fi
 
 contador=0
 
 for i in $(cat procesos.txt | awk '{print $2}'); do
 	if [ $i -gt $1 ]; then
-		contador=$contador+1
+		contador=$(($contador+1))
 	fi
 done
 
 if [ $contador -eq 0 ]; then
-	echo ""
+	echo "No hay ningún PID mayor."
+else
+	echo "Hay un total de $contador PIDs mayores a $1"
 fi
